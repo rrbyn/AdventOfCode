ea44c8160edbd18b4c5a67b55ce089c99cf4ae08
rrbyn <55434239+rrbyn@users.noreply.github.com>
2019-12-04 13:41:56 +0200

    Add files via upload

--- adventofcode2.py
+++ adventofcode2.py
@@ -1,11 +1,20 @@
 f= open("input2.txt","r")
 counter = 0
-
 for x in f:
     opcode = (x.split(","))
-    for x in opcode[counter:(counter + 4)]:
-        if opcode[counter] == 1:
-            opcode [counter + 3] = opcode[counter + 1] + opcode [counter + 2]
-        elif opcode[counter] == 2:
-            opcode [counter + 3] = opcode [counter + 1] * opcode[counter + 3]
-print (opcode)    
+    opcode = list(map(int, opcode))
+    print (opcode[counter])
+    print (opcode)
+for x in opcode:
+    if  opcode[counter] == 1:
+        opcode[opcode[counter + 3]] = opcode[opcode[counter + 1]] + opcode[opcode[counter + 2]]
+        counter = counter + 4
+    elif opcode[counter] == 2:
+        opcode[opcode[counter + 3]] = opcode [counter + 1] * opcode[counter + 2]
+        counter = counter + 4
+    elif opcode[counter] == 99:
+        print ("found 99")
+        break
+    else:
+        print ("error")
+print (opcode)
